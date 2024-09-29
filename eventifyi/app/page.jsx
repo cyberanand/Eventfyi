b299a949cd86b631e8d34dd14328959323554595
cyberanand <98548486+cyberanand@users.noreply.github.com>
2024-09-29 12:14:45 +0530

    changes

--- eventifyi/app/page.jsx
+++ eventifyi/app/page.jsx
@@ -1,12 +1,22 @@
-import { SignForm } from "@/components/SignForm";
+import DiscoverSection from '@/components/HomePage/DiscoverSection';
+// import EventsCarousel from '@/components/HomePage/EventsCarousel';
+import Footer from '@/components/HomePage/Footer';
+import Navbar from '@/components/HomePage/Navbar';
+import SearchBar from '@/components/HomePage/SearchBar';
+import ServiceGrid from '@/components/HomePage/ServiceGrid';
+import TestimonialCarousel from '@/components/HomePage/TestimonialCarousel';
 
 export default function Home() {
   return (
     <main>
+      <Navbar />
+      <SearchBar />
 
-
-      <SignForm></SignForm>
-
+      <DiscoverSection />
+      <ServiceGrid />
+      <TestimonialCarousel />
+      {/* <EventsCarousel /> */}
+      <Footer />
     </main>
   );
 }
